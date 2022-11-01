59b5612bf55fd5a850ab5ce709d1fccb165ee89e
i-nazuma <abraham1999@gmail.com>
2022-11-01 17:33:01 +0100

    first commit

---
+++ Program.fs
@@ -0,0 +1,15 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+open System
+open System.IO
+
+let lines = File.ReadAllLines(__SOURCE_DIRECTORY__ + "/samples/easy.txt")
+let list = Seq.toList lines
+
+let reduce x =
+        x |> List.fold (fun m x -> match Map.tryFind x m with
+                                   | None -> Map.add x 1 m
+                                   | Some c -> Map.add x (c+1) m)
+                       Map.empty
+
+let result = reduce list
+printfn "%A" result
